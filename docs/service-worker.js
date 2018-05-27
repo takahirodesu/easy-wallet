a038bef8bbb9a6b490d803bd9f96685adc6fcf13
takahirodesu <positivezombie1013@gmail.com>
2018-05-28 01:11:13 +0900

    test2

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.c118db7515edb059248d1ba46d1daada.js"
+  "./precache-manifest.8a9d87b8dd1e5829f36d8def525fd5f8.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "easy-wallet"});
@@ -27,6 +27,19 @@ workbox.clientsClaim();
  * requests for URLs in the manifest.
  * See https://goo.gl/S9QRab
  */
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
+self.__precacheManifest = [
+  {
+    "url": "index.html",
+    "revision": "f1c7a1ff780e8ddc62898c8c4710d47c"
+  },
+  {
+    "url": "precache-manifest.c118db7515edb059248d1ba46d1daada.js",
+    "revision": "c118db7515edb059248d1ba46d1daada"
+  },
+  {
+    "url": "service-worker.js",
+    "revision": "40ae9a3038684217cfc33b95a20a3405"
+  }
+].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
