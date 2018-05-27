667d7fa6ddd907d874386bda12f0cc9b0930525d
takahirodesu <positivezombie1013@gmail.com>
2018-05-28 01:44:06 +0900

    a

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.8a9d87b8dd1e5829f36d8def525fd5f8.js"
+  "./precache-manifest.42c5a89e0f08148b96e0755119e3bf84.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "easy-wallet"});
@@ -33,12 +33,16 @@ self.__precacheManifest = [
     "revision": "f1c7a1ff780e8ddc62898c8c4710d47c"
   },
   {
+    "url": "precache-manifest.8a9d87b8dd1e5829f36d8def525fd5f8.js",
+    "revision": "8a9d87b8dd1e5829f36d8def525fd5f8"
+  },
+  {
     "url": "precache-manifest.c118db7515edb059248d1ba46d1daada.js",
     "revision": "c118db7515edb059248d1ba46d1daada"
   },
   {
     "url": "service-worker.js",
-    "revision": "40ae9a3038684217cfc33b95a20a3405"
+    "revision": "e5d3762469ce17caae3e71cc0dd70bff"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
